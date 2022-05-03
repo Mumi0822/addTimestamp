cc9e0b5a2cd2c55ae2f7da87b9e806edc758a9b2
Mumi0822 <ykmail1409@gmail.com>
2022-05-04 00:25:56 +0900

    基本機能完成？

--- namechange.py
+++ namechange.py
@@ -1,20 +1,15 @@
+from email.mime import base
+import imp
 import os
 import pathlib
 import datetime
+import pprint
 import time
 import platform
-from PIL import Image
-from PIL.ExifTags import TAGS
-#p = pathlib.Path('F:\\files\\picture\\VRChat\\2022-04\\VRChat_1920x1080_2022-04-01_00-45-46.858.png')
-#print(os.stat('sample.png'))
-# os.stat_result(st_mode=33188, st_ino=8728494137, st_dev=16777220, st_nlink=1, st_uid=501, st_gid=20, st_size=6, st_atime=1549094615, st_mtime=1549094615, st_ctime=1549094615)
+import re
+from PIL import Image, ExifTags
+import piexif
 
-#print(type(os.stat('sample.png')))
-# <class 'os.stat_result'>
-#dt = datetime.datetime.fromtimestamp(p.stat().st_ctime)
-#print(dt)
-# #for id, value in exif.items():
-#   print(id, TAGS.get(id), value)
 def PtoJ(input_path,output_dir):
    basename  = os.path.basename(input_path)
    im = Image.open(input_path)  
@@ -23,15 +18,38 @@ def PtoJ(input_path,output_dir):
    im2.save(output_path, "JPEG", quality=95)#クオリティ変えた方がいい？
    print(basename,'is converted to JPEG')
 
-def AddExif(input_path,output_dir):
+def GetDT(basename):
+   stamp_list = re.split('[_.]',basename)
+   the_date = stamp_list[2]
+   the_time = stamp_list[3]
+   the_date = the_date.split('-')
+   the_time = the_time.split('-')
+   the_dt = datetime.datetime(int(the_date[0]), int(the_date[1]), int(the_date[2]), int(the_time[0]), int(the_time[1]), int(the_time[2]),int(stamp_list[4]))
+   return the_dt
+   
+
+
+def AddExif(input_path):
    basename = os.path.basename(input_path)
    basename = basename[:-4]
-   stamp_list = basename.split('_')
-   for stamp in stamp_list:
-      print(stamp)
+   dt = GetDT(basename)
+   exif_dict = {}
+   zeroth_ifd={}
+   exif_ifd ={
+      piexif.ExifIFD.DateTimeOriginal: dt.strftime("%Y:%m:%d %H:%M:%S"),
+      piexif.ExifIFD.DateTimeDigitized: dt.strftime("%Y:%m:%d %H:%M:%S")} 
+   gps = {}
+   first_ifd={}
+   exif_dict = {"0th":zeroth_ifd, "Exif":exif_ifd, "GPS":gps, "1st":first_ifd}
+   exif_dict["Exif"]=exif_ifd
+   exif_dict["GPS"] = gps
+   exif_bytes = piexif.dump(exif_dict)
+   piexif.insert(exif_bytes,input_path)
    print('Add TimeStamp to',basename)
 
 
+
 p = os.getcwd()
-p2 = p +'\\VRChat_1920x1080_2022-03-04_22-11-26.015.png' 
-AddExif(p2,p)
+p2 = p +'\\VRChat_1920x1080_2022-03-04_22-11-26.015.jpg' 
+
+AddExif(p2)
